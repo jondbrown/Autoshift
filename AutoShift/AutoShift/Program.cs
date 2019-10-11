cc64c470fe79e8ba55d305c3d1793d9d58448d7d
Jon Brown <jondbrown@gmail.com>
2019-10-10 21:45:58 -0400

    First Login in

--- AutoShift/AutoShift/Program.cs
+++ AutoShift/AutoShift/Program.cs
@@ -4,9 +4,11 @@ namespace AutoShift
 {
     class Program
     {
-        static void Main(string[] args)
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var shiftClient = new Shift();
+            await shiftClient.LoginAsync("jondbrown@gmail.com", "Xenotrain1");
+            await shiftClient.GetRedeemForm("12345-12345-12345-12345-12345");
         }
     }
 }
---
+++ AutoShift/AutoShift/Shift.cs
@@ -0,0 +1,74 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AutoShift
+{
+    class Shift
+    {
+        private static CookieContainer cookieContainer;
+        private static HttpClientHandler clienthandler;
+        private string cookies;
+        private string LatestToken;
+        private HttpClient client;
+        private const string baseUrl = "https://shift.gearboxsoftware.com";
+        public Shift()
+        {
+            cookieContainer = new CookieContainer();
+            clienthandler = new HttpClientHandler { AllowAutoRedirect = true, UseCookies = true, CookieContainer = cookieContainer };
+            client = new HttpClient(clienthandler);
+        }
+
+        private async Task<string> GetTokenAsync(string url)
+        {
+            var response = await client.GetAsync(url);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var content = response.Content.ReadAsStreamAsync();
+                var doc = new HtmlDocument();
+                doc.Load(await content);
+                var meta = doc.DocumentNode.SelectSingleNode("/html/head/meta[@name='csrf-token']");
+                var token = meta.GetAttributeValue("content", "no");
+                return token;
+            }
+            
+            return "";
+        }
+
+        public async Task<bool> LoginAsync(string user, string password)
+        {
+            var uri = new Uri($"{baseUrl}/sessions");
+            var values = new Dictionary<string, string>
+            {
+                { "token", await GetTokenAsync($"{baseUrl}/home") },
+                { "user[email]", user},
+                { "user[password]", password },
+            };
+            var content = new FormUrlEncodedContent(values);
+            var response = await client.PostAsync(uri, content);
+            var responseContent = response.Content.ReadAsStreamAsync();
+            var doc = new HtmlDocument();
+            doc.Load(await responseContent);
+            return false;
+        }
+
+        public async Task<bool> GetRedeemForm(string code)
+        {
+            var token = await GetTokenAsync($"{baseUrl}/code_redemptions/new");
+            client.DefaultRequestHeaders.Add("X-CSRF-Token", token);
+            client.DefaultRequestHeaders.Add("X-Requested-With", "XMLHttpRequest");
+            var response = await client.GetAsync($"{baseUrl}/entitlement_offer_codes?code={code}");
+            var responseContent =  response.Content.ReadAsStreamAsync();
+            var doc = new HtmlDocument();
+            doc.Load(await responseContent);
+            return false;
+        }
+    }
+}
